7c5fccc03f02936f36ac0f709c6f79b8b6013bd7
zakiya adam <zakiyaadan12@gmail.com>
2022-03-15 21:32:34 +0300

    used *ngfor in thehome component for showing the images inside the view

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -9,15 +9,15 @@ export class FoodService {
 
   getAll():String[]{
     return [
-      '/assets/images/photo1.jpeg',
-      '/assets/images/photo2.jpeg',
-      '/assets/images/photo3.jpeg',
-      '/assets/images/photo4.jpeg',
-      '/assets/images/photo5.jpeg',
-      '/assets/images/photo6.jpeg',
-      '/assets/images/photo7.jpeg',
-      '/assets/images/photo8.jpeg',
-      '/assets/images/photo9.jpeg',
+      '/assets/images/food/photo1.jpeg',
+      '/assets/images/food/photo2.jpeg',
+      '/assets/images/food/photo3.jpeg',
+      '/assets/images/food/photo4.jpeg',
+      '/assets/images/food/photo5.jpeg',
+      '/assets/images/food/photo6.jpeg',
+      '/assets/images/food/photo7.jpeg',
+      '/assets/images/food/photo8.jpeg',
+      '/assets/images/food/photo9.jpeg',
     ]
   }
 }
